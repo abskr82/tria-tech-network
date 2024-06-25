bbec2a1e4f8d990c53a25cb7964e5215de06bc00
abskr82 <abhishekkumar647@outlook.com>
2024-06-25 12:41:21 +0530

    adding setup and initial files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+import './App.css'
+import Header from './Components/Header/Header';
+import ContentPage from './Components/Content/Content';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ContentPage />
+    </>
+  )
+}
+
+export default App
